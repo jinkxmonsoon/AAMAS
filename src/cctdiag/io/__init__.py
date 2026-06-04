6238bf01226222f76f08511bd47713fddcb6219f
Lucas Castro <lucasgmuniz@gmail.com>
2026-06-03 23:47:23 -0400

    Freeze full-trace schema migration plan

--- src/cctdiag/io/__init__.py
+++ src/cctdiag/io/__init__.py
@@ -1,1 +1,11 @@
-"""Placeholder subpackage: cctdiag.io (initialization phase)."""
+"""Input/output helpers."""
+
+from cctdiag.io.loaders import load_jsonl
+from cctdiag.io.views import assert_no_gold_leakage, make_prediction_view, make_private_label_view
+
+__all__ = [
+    "assert_no_gold_leakage",
+    "load_jsonl",
+    "make_prediction_view",
+    "make_private_label_view",
+]
